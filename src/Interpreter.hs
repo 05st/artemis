976ec5e3ba0235673194df243918e4f42344690e
05st <05st@users.noreply.github.com>
2021-08-07 10:42:15 -0500

    Occurs check, type inference bug fixes

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -41,7 +41,7 @@ addConst a b = tell [CEquality a b]
 typecheck :: [Stmt] -> Either TypeError String
 typecheck stmts = case runExcept (runRWST (checkProgram stmts) [] (Data.Map.empty, 0)) of
     Left err -> Left err
-    Right (_, (s, _), c) -> Right $ '\n':show (runSolve c s) ++ " | \n\n" ++ show c ++ " | \n\n" ++ show s
+    Right (_, (s, _), c) -> Right $ '\n':show (runSolve c s) ++ " | \n\n" ++ show c
 
 checkProgram :: [Stmt] -> Infer ()
 checkProgram ((SExpr e) : stmts) = inferExpr e >> checkProgram stmts
@@ -120,14 +120,11 @@ inferItem = \case
         return (TFunc pt rt)
     IUnit -> return tUnit
 
-substitute :: Type -> ExceptT TypeError (State Subst) Type
-substitute = \case
-    a@TCon {} -> return a
-    a@(TVar i) -> get >>= \s -> return $ fromMaybe a (Data.Map.lookup i s)
-    (TFunc a b) -> do
-        p <- substitute a
-        r <- substitute b
-        return $ TFunc p r
+substitute :: Subst -> Type -> Type
+substitute s = \case
+    a@TCon {} -> a
+    a@(TVar i) -> fromMaybe a (Data.Map.lookup i s)
+    (TFunc a b) -> TFunc (substitute s a) (substitute s b)
 
 tvs :: Type -> Set Type
 tvs (TCon _) = Data.Set.empty
@@ -139,16 +136,23 @@ occurs a b = a `Data.Set.member` tvs b
 
 unify :: Type -> Type -> ExceptT TypeError (State Subst) ()
 unify a b | a == b = return ()
-unify a@(TVar i) b = get >>= put . Data.Map.insert i b
-unify a b@(TVar i) = get >>= put . Data.Map.insert i a
+unify (TVar i) b = bind i b 
+unify a (TVar i) = bind i a
 unify (TFunc fa fb) (TFunc ga gb) = unify fa ga >> unify fb gb
 unify a b = throwError $ Mismatch a b
 
+bind :: Int -> Type -> ExceptT TypeError (State Subst) ()
+bind i t | t == a = return ()
+         | occurs a t = throwError $ InfiniteType a t
+         | otherwise = get >>= put . Data.Map.insert i t
+    where a = TVar i
+
 solve :: [Constraint] -> ExceptT TypeError (State Subst) Subst
 solve ((CEquality a b) : cs) = do
-    a' <- substitute a
-    b' <- substitute b
-    unify a' b'
+    s <- get
+    unify (substitute s a) (substitute s b)
+    s' <- get
+    put (Data.Map.map (substitute s') s')
     solve cs
 solve [] = get
 
@@ -156,61 +160,3 @@ runSolve :: [Constraint] -> Subst -> Either TypeError Subst
 runSolve c s = case runState (runExceptT (solve c)) s of
     (Left err, _) -> Left err
     (Right _, s) -> Right s
-
-{-
-
-sub :: Subst -> Type -> Type
-sub s = \case
-    TCon a -> TCon a
-    t@(TVar i) -> Data.Map.findWithDefault t i s
-    (TFunc a b) -> TFunc (sub s a) (sub s b)
-
-unify :: Type -> Type -> ExceptT TypeError (State Subst) Subst
-unify (TFunc a b) (TFunc a' b') = do
-    s1 <- unify a a'
-    s2 <- unify (sub s1 b) (sub s1 b')
-    return (Data.Map.map (sub s1) s2 `Data.Map.union` s1)
-unify a@(TVar _) b = bind a b
-unify a b@(TVar _) = bind b a
-unify (TCon a) (TCon b) | a == b = return Data.Map.empty
-unify a b = throwError $ UnifyError a b
-
-bind :: Type -> Type -> ExceptT TypeError (State Subst) Subst
-bind a@(TVar i) b
-    | a == b = return Data.Map.empty
-    | occurs a b = throwError $ InfiniteType a b
-    | otherwise = return $ Data.Map.singleton i b
-bind a b = throwError $ UnifyError a b
-
-
-solve :: [Constraint] -> Subst -> Either TypeError Subst
-solve stmts subst = case runState (runExceptT (go stmts)) subst of
-        (Left err, _) -> Left err
-        (Right _, s) -> Right s
-    where
-        go ((CEquality a b) : cs) = go cs >>= Data.Map.union (unify a b)
-        go [] = get
-subAll :: Type -> Type -> Type -> Type
-subAll a b t | t == a = b
-subAll a b (TFunc fa fb) = TFunc (subAll a b fa) (subAll a b fb)
-subAll a b t = t
-
-unify :: Type -> Type -> ExceptT TypeError (State Subst) ()
-unify a b | a == b = return ()
-unify at@(TVar i) b = do
-    s <- get
-    case Data.Map.lookup i s of
-        Nothing -> throwError $ NotDefined ('$':show i)
-        Just t -> if t /= at
-            then unify t b 
-            else put $ Data.Map.map (subAll t b) $ insert i b s
-unify a bt@(TVar i) = do
-    s <- get
-    case Data.Map.lookup i s of
-        Nothing -> throwError $ NotDefined ('$':show i)
-        Just t -> if t /= bt
-            then unify t a 
-            else put $ Data.Map.map (subAll t a) $ insert i a s
-unify (TFunc fa fb) (TFunc ga gb) = unify fa ga >> unify fb gb
-unify a b = throwError $ Mismatch a b
--}
